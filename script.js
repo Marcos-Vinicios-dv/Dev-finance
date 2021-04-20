9970454a86a4711eb6c35416ef424bfffd3a8964
Marcos Vinicios Teixira <marcosviniciosdev13@gmail.com>
2021-04-19 22:22:17 -0300

    Removendo Comentarios

--- script.js
+++ script.js
@@ -1,79 +1,49 @@
 const Modal = {
     open() {
-        // Selector serve para pegar uma classe css e com classList eu encontro 
         document.querySelector('.modal-overlay').classList.add('active');
-
     },
     close() {
-        // PESQUISAR A FUNÇÃO TOGGLE CLASSLIST
         document.querySelector('.modal-overlay').classList.remove('active');
     }
 }
 
 const Storage = {
     get() {
-        // com o JSON.parse convertemos de volta em array ou objeto
         return JSON.parse(localStorage.getItem("dev.finance:transactions")) || [];
-
     },
     set(transactions) {
-
-        // para armazenarmos no localStorage um array inteiro precisamos comverte-lo
-        // em string usando JSON.stringfy()
         localStorage.setItem("dev.finance:transactions", JSON.stringify(transactions));
-
     }
 }
 
-// primeira coisa a se fazer, CRIAR AS FUNCIONALIDADES
-
-//remover das entradas o valor das saídas
-//assim eu terei o total
-
-//objetos com letra maiuscula
 const Transaction = {
-    // refatorando o codigo
     all: Storage.get(),
     add(transaction) {
-
         Transaction.all.push(transaction);
 
         App.reload();
-
     },
     remove(index) {
-
         Transaction.all.splice(index, 1);
 
         App.reload();
-
     },
     incomes() {
-
         let income = 0;
-        //pepgar todas as transações
-        // para cada transação
         Transaction.all.forEach((transaction) => {
-            // se ela for maior que zero
             if (transaction.amount > 0) {
                 income += transaction.amount;
             }
         })
-        // retornar variable
         return income;
     },
     expenses() {
-
         let expense = 0;
-        //pepgar todas as transações
-        // para cada transação
         Transaction.all.forEach((transaction) => {
-            // se ela for menor que zero
             if (transaction.amount < 0) {
                 expense += transaction.amount;
             }
         })
-        // retornar variabl
         return expense;
     },
     total() {
@@ -81,24 +51,16 @@ const Transaction = {
     }
 };
 
-// Substituir os dados do HTML com os dados do JS
-
 const DOM = {
     transactionsContainer: document.querySelector('#data-table tbody'),
 
     addTransaction(transaction, index) {
-        // createElement cria uma tag html no js
         const tr = document.createElement('tr')
-        // nele podemos usar um método para inserir um html nele
         tr.innerHTML = DOM.inneHTMLTransaction(transaction, index)
-        // em seguida chamamos a função que esta dentro do objeto DOM
 
         tr.dataset.index = index;
 
-        // com o container de mudanças selecionado vamos de
-        // vamos alterar seu conteúdo usando o método appendChild
         DOM.transactionsContainer.appendChild(tr);
-        // e introduzir no nosso container o elemnto/tag html que criamos acima
     },
     inneHTMLTransaction(transaction, index) {
 
@@ -120,17 +82,12 @@ const DOM = {
         return html;
     },
     updateBalance() {
-        // usando o getElementById pegamos qualquer elemento do HTML com
-        // id especificado, e com o innerHTML inserimos o valor que 
-        // queremos no lugar do que ja tinha
         document.getElementById('incomeDisplay').innerHTML = Utils.formatCurrency(Transaction.incomes());
         document.getElementById('expenseDisplay').innerHTML = Utils.formatCurrency(Transaction.expenses());
         document.getElementById('totalDisplay').innerHTML = Utils.formatCurrency(Transaction.total());
     },
     clearTransactions() {
-
         DOM.transactionsContainer.innerHTML = '';
-
     },
 }
 
@@ -158,24 +115,14 @@ const Utils = {
         })
 
         return signal + value;
-
-        // dentro do replace e provavelmente de outros métodos podemos usar
-        // uma expressão regular " /qualquer valor(que se repita ou não)/g "
-        // com esse "g(de global)" o replace sera executado para a string inteira
-        // value = String(value).replace(/0/g, "test")
-
-        // expressão regular para encontrar tudo que não é numero
-        // value = String(value).replace(/\D/g, "test")
     },
 }
 
 const Form = {
-    // como no css eu pego o id e o elemento
     description: document.querySelector('input#description'),
     amount: document.querySelector('input#amount'),
     date: document.querySelector('input#date'),
 
-    // agora eu quero apenas os valores não o elemento todo
     getValues() {
         return {
             description: Form.description.value,
@@ -186,15 +133,12 @@ const Form = {
     validateFields() {
 
         const { description, amount, date } = Form.getValues();
-        // o trim serve para fazer uma limpeza nos espaços vazios
         if (description.trim() === "" || amount.trim() === "" || date.trim() === "") {
             throw new Error("Por favor preencha todos os campos")
         }
 
     },
     formatValues() {
-
-        // aqui não usamos o const pois vamos alterar esses valores
         let { description, amount, date } = Form.getValues();
 
         amount = Utils.formatAmount(amount);
@@ -202,7 +146,6 @@ const Form = {
         date = Utils.formatDate(date);
 
         return { description, amount, date };
-
     },
     clearFields() {
 
@@ -212,23 +155,17 @@ const Form = {
 
     },
     submit(event) {
-        event.preventDefault();// estou dizendo para não executar o evento padrã
+        event.preventDefault();
 
         try {
-
-            // verificar se todas as infomações foram preenchidas
             Form.validateFields();
 
-            // formatar os ddados para salvar
             const transaction = Form.formatValues();
 
-            // salvar 
             Transaction.add(transaction);
 
-            // apagar os dados do formulario
             Form.clearFields();
 
-            // fechar modal
             Modal.close();
 
         } catch (error) {
@@ -240,14 +177,11 @@ const Form = {
 
 const App = {
     init() {
-
-        // o método forEach aplica uma função para cada elemetno do array/elemtno
         Transaction.all.forEach(DOM.addTransaction)
 
         DOM.updateBalance();
 
         Storage.set(Transaction.all)
-
     },
     reload() {
 
